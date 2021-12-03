023bbb3633151b212d743039dfc45167f2277a76
Gruodis <43164261+Gruodis@users.noreply.github.com>
2021-12-03 19:24:37 +0200

    Ciklai penktadienio uzdaviniai

--- 003 ciklai/ciklai.js
+++ 003 ciklai/ciklai.js
@@ -1,3 +1,8 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 //ciklas yra pasikartojimas sklaistu viduje
 
 //ciklo kintamojo deklaravimas, kad butu aisku kiek kartu pakartoti
@@ -65,4 +70,112 @@ for(let i = 0; i <= 8; i++){
 
 };
 console.log('//')
-console.log('// 6 end //')
+console.log('// 6 end //');
+
+//7.
+for (let i = 0; i < 5; i++) {
+
+    console.log(rand(0, 10));
+
+};
+console.log('//')
+console.log('// 7 end //');
+
+//8.
+
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+
+let skaicius1 = rand(5, 25);
+let skaicius2 = rand(5, 25);
+let skaicius3 = rand(5, 25);
+
+let sum;
+
+sum = skaicius1 + skaicius2 + skaicius3;
+console.log(`************ 1 uzdavinys END. suma: ${sum}`);
+
+console.log('//')
+console.log('// 1A end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+
+let sumString; // kintamojo vardas sudaromas pagal "camel case variable" metoda
+
+sumString = ' ' + skaicius1 + ' - ' + skaicius2 + ' - ' + skaicius3;
+console.log(`************ 1 uzdavinys END. suma: ${sumString}`);
+
+console.log('//')
+console.log('// 1B end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+
+let sumString2; // kintamojo vardas sudaromas pagal "camel case variable" metoda
+
+sumString2 = ' ' + skaicius1 + ' - ' + skaicius2 + ' - ' + skaicius3;
+console.log(`************ 1 uzdavinys END. suma: ${sumString2}`);
+
+console.log('//')
+console.log('// 1C end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+
+let sumString3; // kintamojo vardas sudaromas pagal "camel case variable" metoda
+
+sumString3 = ' ' + skaicius1 + ' - ' + skaicius2 + ' - ' + skaicius3 + ' = ' + sum;
+console.log(`************ 1 uzdavinys END. suma: ${sumString3}`);
+
+console.log('//')
+console.log('// 1D end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+
+let numberis = rand(5, 10);
+
+console.log('skaicius sugeneruotas', numberis);
+
+console.log('//')
+console.log('// Sugeneruotas end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+let kintamasisLabas = 'Labas Pupa';
+
+for(let i = 0; i < 5; i++){
+
+    console.log(i, kintamasisLabas)
+
+};
+
+console.log('//')
+console.log('// Sugeneruotas LabasPupa end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+
+for(let i = 0; i < 7; i++){
+
+    console.log(sumString2)
+
+};
+
+console.log('//')
+console.log('// Sugeneruotas 7 kartus suma end //');
+
+/////////////////////////////////////////////////////////// Kiti uzdaviniai ///////////////////////////////////////////////////////
+
+for(let i = 0; i < numberis; i++){
+
+    console.log(numberis)
+
+};
+
+console.log('//')
+console.log('// Atspausdintas tiek kartu koks skaicius sugeneruotas end //');
