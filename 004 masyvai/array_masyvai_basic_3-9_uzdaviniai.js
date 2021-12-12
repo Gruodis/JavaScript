233b1567cd7caf78c0e4a034d2be02469f9e907f
Gruodis <43164261+Gruodis@users.noreply.github.com>
2021-12-12 15:42:24 +0200

    3 Uzdavinio Sprendimas

--- 004 masyvai/array_masyvai_basic_3-9_uzdaviniai.js
+++ 004 masyvai/array_masyvai_basic_3-9_uzdaviniai.js
@@ -35,9 +35,7 @@ function rand(min, max) {
 //
 //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-// let randomRaides = rand(0, 3) ? (rand(0, 2) ? (rand(0, 1) ? 'A' : 'B') : 'C') : 'D';
-
-let arrayThree = []; // deklaruotas Objektas "arrayThree";
+let arrayThree = []; // deklaruotas Objektas(Masyvas) "arrayThree";
 
 let randomNumberOfElements = rand(1, 200); // generuojame atsitiktini Elementu skaiciu Objektui "arrayThree";
 
@@ -46,22 +44,28 @@ let skaiciuojamRaidesB = 0; // deklaruotas kintamasis, kuriame sumuosime raides 
 let skaiciuojamRaidesC = 0; // deklaruotas kintamasis, kuriame sumuosime raides C;
 let skaiciuojamRaidesD = 0; // deklaruotas kintamasis, kuriame sumuosime raides D;
 
-let randomRaides; // deklaruojame kintamaji kuris atpazins raide Elemento viduje(Stalciuje);
 
-for (let i = 0; i <= randomNumberOfElements; i++) { // kartojame cikla, kol bus pasiektas "randomNumberOfElements" esantis skaicius;
+for (let i = 0; i <= randomNumberOfElements; i++) { // kartojame cikla, kol bus pasiektas "randomNumberOfElements" skaicius;
 
-    arrayThree.push(randomRaides = rand(0, 3) ? (rand(0, 2) ? (rand(0, 1) ? 'A' : 'B') : 'C') : 'D'); // kartojant cikla, generuojame atsitiktine raide;
 
-    if (randomRaides === 'A') {      // patikriname ar sugeneruota raide yra A;
+    // !!!! TOKS DEKLARAVIMAS BETIKSLIS, TODEL ISKART DEDAME I arrayThree.push(); SALYGOS VIDU !!!!!!!!!!!!!!!!!!!!!!
+    // --------------------------------------------------------------------------------------------------------------
+    // let randomRaides = rand(0, 3) ? (rand(0, 2) ? (rand(0, 1) ? 'A' : 'B') : 'C') : 'D'; // deklaruojame kintamaji, 
+    // kuris generuos A, B, C arba D raide ir bus naudojamas arrayThree.push(randomRaides);
+
+
+    arrayThree.push(rand(0, 3) ? (rand(0, 2) ? (rand(0, 1) ? 'A' : 'B') : 'C') : 'D'); // sugeneruota atsitiktine raide dedame i Elemento viduju(Stalciu);
+
+    if (arrayThree[i] === 'A') {      // patikriname ar sugeneruota raide = A;
         skaiciuojamRaidesA++;        // Skaiciuojame raides A;
     }
-    else if (randomRaides === 'B') { // patikriname ar sugeneruota raide yra B;
+    else if (arrayThree[i] === 'B') { // patikriname ar sugeneruota raide = B;
         skaiciuojamRaidesB++;        // Skaiciuojame raides B;
     }
-    else if (randomRaides === 'C') { // patikriname ar sugeneruota raide yra C;
+    else if (arrayThree[i] === 'C') { // patikriname ar sugeneruota raide = C;
         skaiciuojamRaidesC++;        // Skaiciuojame raides C;
     }
-    else {                           // jeigu auksciau esantys IF'ai neatitinka uzduotos salygos( raide === raide );
+    else {                           // jeigu auksciau esantys IF'ai neatitinka uzduotos salygos( randomRaide = raide );
         skaiciuojamRaidesD++;        // Skaiciuojame raides D, 
     }
 
@@ -82,8 +86,40 @@ console.log(`||
 console.log(`||
 ||  Skaiciuojam raides - D:`, skaiciuojamRaidesD);
 
+//////////////////------------------- DESTYTOJO SPRENDIMAS--------------------  ////////////////////////
+//                                                                                                    //
+//                                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// 3.	Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, D ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
+// Sprendimui naudojame du Objektus(masyvus): Objekta - "raides" ir Objekta - "arrayThree".
+// Pirmame laikysime raides, antrame - Ciklo FOR pagalba iteruosime reikiama Elementu kieki su atsitiktinemis raidemis is pirmojo Objekto.
+
+// Papildomame Objekte "raides", turime: Elentas Nr.0 = 'A', Elementas Nr.1 = 'B', Elementas Nr.2 = 'C', Elementas Nr.3 = 'D';
+let raides = ['A', 'B', 'C', 'D']; // deklaruotas papildomas Objektas(masyvas);
+
+// 1. - Generuojame random skaiciu nuo 0 iki 3 (pvz. sugeneravome skaiciu 2.),
+// 2. - Objekte "raides", randame Elementa, kurio Nr. atitiko random skaiciu(2), tokiu budu radome Elementas Nr.2 (kurio reiksme 'C');
+// 3. - Tuomet antrame Objekte "arrayThree", vykdome PUSH naujas Elementas, kurio reiksme = Elentas Nr.(random skaicius) is Objekto "raides";
+let arrayThree2 = []; // deklaruotas Objektas(Masyvas),
+
+//////////////////////// Objekto(Masyvo) Elementams atsitiktine tvarka bus priskiriamos reiksmes "'A', 'B', 'C'..",
+//////////////////////// kurias imsime is auksciau deklaruoto Objekto(Masyvo) "raides" = ['A', 'B',...];
+
+for (let i = 0; i <= randomNumberOfElements; i++) {
+
+    arrayThree2.push( // Objekto(Masyvo) arrayThree2.push viduje, idedame pagalbini Objekta(Masyva) "raides", 
+        raides[       // sekantis zingsnis - [] skliaustuose, kurie simbolizuoja Elemento reiksme,
+        rand(0, 3)    // generuojame skaiciu (nuo 0 iki 3),
+        ]             // pvz.: rand. sk. = 2, tada 2 = 'C', nes 2 = Elemento(Stalciaus) Nr., papildomame Objekte(Masyve) "raides" 
+    ); 
+};
+console.log(`Objekto Elementai su Kintamaisiais:`, arrayThree2)
+console.log(`// 
+Objekto Dydis:`, arrayThree2.length)
+
+
 // 4.	Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. Sudėkite masyvus, sudėdami atitinkamas reikšmes. Paskaičiuokite kiek unikalių (po vieną, nesikartojančių) reikšmių ir kiek unikalių kombinacijų gavote.
 // 5.	Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
 // 6.	Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra pirmame 6 uždavinio masyve, bet nėra antrame 6 uždavinio masyve.
