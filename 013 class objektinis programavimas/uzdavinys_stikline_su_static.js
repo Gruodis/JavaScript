ef69a6af716406112e93d05e46827350a7e818ab
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-01-27 16:13:43 +0200

    Class Stikline DONE

--- 013 class objektinis programavimas/uzdavinys_stikline_su_static.js
+++ 013 class objektinis programavimas/uzdavinys_stikline_su_static.js
@@ -57,106 +57,139 @@ class Stikline {
     // savybes
 
     static pertekliausLikutis = 0;
-    // static ilipoViso = 0;
-    // static islipoViso = 0;
 
     // metodai
     constructor(turis) {
 
 
-        this.gerimas = 0;
-        this.ipilam = turis;
-        // this.kauptiPertekliu = 0;
-        // this.ilipoViso = 0;
-        // this.vaziuoja = 0;
+        this.skystis = 0;
+        this.turis = turis;
+        this.likutis = 0;
 
-        // this.id = rand(2, 19);
 
     }
 
     pripilti = (kiekis) => {
 
-        this.gerimas += kiekis;
-        this.gerimas = Math.min(this.ipilam, this.gerimas)
+        if (kiekis === undefined) {
+            kiekis = 0
+        }
+        console.log('\n Backoje turime', kiekis);
 
-console.log('\n', this.ipilam, this.gerimas);
+
+        this.skystis += kiekis;
+        // this.skystis = Math.min(this.turis, this.skystis)
 
 
+        if (Stikline.pertekliausLikutis < 0) {
+            this.skystis = 0;
+            Stikline.pertekliausLikutis = 0;
+            console.log('\n Pripilti START If < 0  skystis', this.skystis);
+        }
 
-        // if ((kiekis) >= 0) {
 
-        //     Stikline.ilipoViso += kiekis;
-
-        //     this.ilipo = kiekis;
-        //     this.ilipoViso += kiekis;
-
-        //     console.log(`
-        //     |${this.id}| GO:
-        //     Troleibuse yra keleiviu: `, this.vaziuoja, `
-        //     |${this.id}| STOP:
-        //     Ilipo nauju keleiviu: `, this.ilipo, `
-        //     Toliau vaziuoja keleiviu: `, this.vaziuoja += this.ilipo
-        //     );
-
-        //     // console.log('X ', this.vaziuoja)
-
-        // }
-        // else {
-        //     // this.islipo = Math.max(0, (this.vaziavo += kiekis))
-
-        //     if ((this.vaziuoja + kiekis) <= 0) {
-        //         this.islipo = 0;
-
-        //         console.log(`  
-        //         |${this.id}| GO:
-        //         Troleibuse yra keleiviu: `, this.vaziuoja, `
-        //         |${this.id}| STOP:
-        //         Troleibus yra keleiviu: `, this.vaziuoja, `
-        //         Islipo kiekis: `, this.vaziuoja, `
-        //         Toliau vaziuoja: `, 0
-        //         );
-
-        //         this.vaziuoja = 0;
-        //     }
-        //     else {
-        //         console.log(`
-        //     |${this.id}| GO:
-        //     Troleibuse yra keleiviu: `, this.vaziuoja, `
-        //     |${this.id}| STOP: 
-        //     Islipo kiekis: `, kiekis, `
-        //     Liko = `, + (this.vaziuoja += kiekis)
-        //         );
-
-        //     }
+        if (this.skystis > this.turis) {
+            if ((Stikline.pertekliausLikutis === 0)) {
 
 
 
-        // }
+                Stikline.pertekliausLikutis += (this.skystis - this.turis)
+
+                // console.log('\n Backoje liko: ', Stikline.pertekliausLikutis);
+                this.skystis = this.turis;
+                console.log('Pripilam pirma stikline: ', '\n Turis ', this.turis, ' Skystis ', this.skystis, '\n  backoje liko: ', Stikline.pertekliausLikutis);
+
+
+            }
+            else if (Stikline.pertekliausLikutis < 0) {
+                this.skystis = 0;
+                console.log('\n Pripilti turis/skystis', this.turis, this.skystis);
+                console.log('\n  backoje liko: ', Stikline.pertekliausLikutis);
+
+
+            }
+            else {
+                Stikline.pertekliausLikutis -= this.turis;
+
+                this.skystis = this.turis;
+                console.log('Pripilam paskutine: ', '\n Turis ', this.turis, ' Skystis ', this.skystis);
+                console.log('\n Baigus pilstyti backoje liko: ', Stikline.pertekliausLikutis);
+
+
+
+
+            }
+
+
+        }
     }
 
     perpilti = () => {
-        const tarpinisKiekis = this.gerimas;
-        console.log('\n Tara ', this.ipilam, ' kiekis', this.gerimas);
 
-        this.gerimas = 0;
-        return tarpinisKiekis;
+        if (Stikline.pertekliausLikutis < 0) {
+            this.skystis = 0;
+            Stikline.pertekliausLikutis = 0;
+            console.log('\n Perpilti START If < 0  skystis', this.skystis);
+
+        }
+
+        this.skystis = Stikline.pertekliausLikutis;
+
+        if (Stikline.pertekliausLikutis > 0) {
+
+            if (this.turis > this.skystis) {
+                this.skystis = Stikline.pertekliausLikutis;
+                console.log('\n 8 If ++ 0  skystis', 'Skystis: ', this.skystis, ' Turis: ', this.turis);
+
+
+                if (Stikline.pertekliausLikutis -= this.turis < 0 || Stikline.pertekliausLikutis < this.turis) {
+                    Stikline.pertekliausLikutis = 0;
+                }
+                else {
+                    Stikline.pertekliausLikutis -= this.turis;
+
+                }
+                console.log('\n 9 If -- 0  skystis', 'Skystis: ', this.skystis, ' Turis: ', this.turis);
+
+            }
+            else {
+
+                this.skystis = this.turis;
+                console.log('\n Pripilti turis/skystis', this.turis, this.skystis);
+
+                if (Stikline.pertekliausLikutis < this.turis) {
+                    Stikline.pertekliausLikutis = 0;
+
+                }
+                else {
+                    Stikline.pertekliausLikutis -= this.turis;
+
+                }
+                console.log('\n X0X0 backoje liko: ', Stikline.pertekliausLikutis);
+
+            }
+
+            // console.log('Backoje liko: \n ', Stikline.pertekliausLikutis);
+            return Stikline.pertekliausLikutis;
+
+        }
+        else if (Stikline.pertekliausLikutis < 0) {
+            this.skystis = 0;
+            Stikline.pertekliausLikutis = 0;
+            console.log('\n 3 If < 0  skystis', this.skystis);
+
+        }
+
+        else {
+            console.log('\n 4 else last ', this.turis, ' kiekis', this.skystis);
+
+        }
 
     }
 
     visoStiklineje = () => {
-        return this.gerimas;
 
-    
-    }
-
-    galine = () => {
-
-        console.log(`
-        Viso |${this.id}|-to marsruto troleibusu vaziavo: ${this.ilipoViso} keleiviu
-        Galine stoteleje islipo: `, this.vaziuoja, `keleivis(-iu)`);
-
-        Stikline.paskutineStoteleViso += this.vaziuoja;
-
+        console.log('\n Stiklines turis: ', this.turis, '\n Skyscio stiklineje: ', this.skystis);
     }
 
 }
@@ -165,72 +198,29 @@ const stikline200 = new Stikline(200);
 const stikline150 = new Stikline(150);
 const stikline100 = new Stikline(100);
 
-// const keleiviai = [1, 1, -22, 4, -3, -66, 10]
 
-// console.log(`
-// Toleibusas NR. |${marsrutas.id}| 
-// Ilipa/islipa MASYVAS : `, keleiviai);
 
-// keleiviai.forEach(keleivis => {
-//     marsrutas.pripilti(keleivis);
+// stikline200.pripilti(875);
+stikline150.pripilti(475);
 
-// })
 
-stikline200.pripilti(75);
-stikline150.pripilti(stikline200.perpilti());
-stikline100.pripilti(stikline150.perpilti());
 
+// stikline100.pripilti(
+//     stikline150.perpilti(
+//         stikline200.perpilti()
+//     )
+// );
+
+stikline100.pripilti(
+    stikline150.perpilti()
+);
 
 
 console.log('\n');
-console.log('200 ', stikline200.visoStiklineje())
-console.log('150 ', stikline150.visoStiklineje())
-console.log('100 ', stikline100.visoStiklineje())
+stikline200.visoStiklineje()
+stikline150.visoStiklineje()
+stikline100.visoStiklineje()
 
 
-// console.log(`Viso ilipo keleiviu: `, Stikline.ilipoViso, `.`);
-
-// marsrutas.galine()
 console.log('\n');
-
-// const marsrutas2 = new Stikline();
-
-// const keleiviai2 = [10, -2, -7, 4, -3, 2, 10]
-
-// console.log(`
-// Toleibusas NR. |${marsrutas2.id}|
-// Ilipa/islipa MASYVAS : `, keleiviai2);
-
-// keleiviai2.forEach(keleivis => {
-//     marsrutas2.pripilti(keleivis);
-
-// })
-
-// console.log('\n');
-
-// // console.log(`Viso ilipo keleiviu: `, Stikline.ilipoViso, `.`);
-// marsrutas2.galine()
-// console.log('\n');
-
-// const marsrutas3 = new Stikline();
-
-// const keleiviai3 = [10, -6, -4, 8, 4, -10]
-
-// console.log(`
-// Toleibusas NR. |${marsrutas.id}|
-// Ilipa/islipa MASYVAS : `, keleiviai3);
-
-// keleiviai3.forEach(keleivis => {
-//     marsrutas3.pripilti(keleivis);
-
-// })
-
-// console.log('\n');
-
-// // console.log(`Viso ilipo keleiviu: `, Stikline.ilipoViso, `.`);
-
-// marsrutas3.galine()
-// console.log('\n');
-
-
-// console.log(`Per para galineje stoteleje VISO islipo keleiviu: `, Stikline.paskutineStoteleViso, `.`);
+console.log('Backoje liko ', Stikline.pertekliausLikutis)
