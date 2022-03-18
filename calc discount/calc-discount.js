9107a19fb7cabe152aa8a64fe14c5d0d1e1b31cf
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-03-18 14:05:19 +0200

    calc with map

--- calc discount/calc-discount.js
+++ calc discount/calc-discount.js
@@ -1,6 +1,6 @@
 const sell_price = document.querySelectorAll(".ispardavimas");
 const base_price = document.querySelectorAll(".bazine");
-const combine = [sell_price, base_price];
+//const combine = [[sell_price], [base_price]];
 // const sell_price = parseFloat(document.querySelectorAll(".ispardavimas").innerText);
 // const base_price = parseFloat(document.querySelectorAll(".bazine").innerText);
 
@@ -10,16 +10,33 @@ const combine = [sell_price, base_price];
 // )
 console.log(sell_price);
 console.log(base_price);
-console.log(combine);
+// console.log(combine);
 console.log(15);
 
-var datos1 = [5];
-var datos2 = [1];
-var result = datos1.filter(el => !datos2.includes(el));
-console.log(result);
+// var datos1 = [5];
+// var datos2 = [1];
+// var result = datos1.filter(el => !datos2.includes(el));
+// console.log(result);
+
+// console.log(`
 
 
-let sum = sell_price.map(function (num, idx) {
-    return num + base_price[idx];
-}); // [6,8,10,12]
-console.log(sum);
+// `);
+// let sum = sell_price.map(function (num, idx) {
+//     return num + base_price[idx];
+// }); // [6,8,10,12]
+// console.log(sum);
+
+let array1 = [6, 4, 3, 9];
+let array2 = [5, 6, 7, 8];
+
+let suma = array1.map(function (num, idx) {
+    //return num + array2[idx];
+
+    console.log(
+
+        // num + array2[idx]),
+        `NUM `, array1[idx] - array2[idx]
+    )
+}
+); // [6,8,10,12]
