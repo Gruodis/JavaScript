08eb970544fe0a53c4f8ac8989d340f56247e5ad
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-03-19 21:27:28 +0200

    discount done

--- calc discount/calc-discount.js
+++ calc discount/calc-discount.js
@@ -1,17 +1,27 @@
 const sell_price = document.querySelectorAll(".ispardavimas");
 const base_price = document.querySelectorAll(".bazine");
+const tevas = document.querySelectorAll(".akcija");
 //const combine = [[sell_price], [base_price]];
 // const sell_price = parseFloat(document.querySelectorAll(".ispardavimas").innerText);
 // const base_price = parseFloat(document.querySelectorAll(".bazine").innerText);
+let array1 = [];
+let array2 = [];
 
-// sell_price.forEach(price => {
-//     console.log(price.innerText)
-// }
-// )
+sell_price.forEach(price => {
+    array1.push(price.innerText)
+}
+)
+base_price.forEach(price => {
+    array2.push(price.innerText)
+}
+)
 console.log(sell_price);
 console.log(base_price);
+console.log(tevas);
 // console.log(combine);
 console.log(15);
+console.log(`ARRAY1 `, array1);
+console.log(`ARRAY2 `, array2);
 
 // var datos1 = [5];
 // var datos2 = [1];
@@ -27,16 +37,39 @@ console.log(15);
 // }); // [6,8,10,12]
 // console.log(sum);
 
-let array1 = [6, 4, 3, 9];
-let array2 = [5, 6, 7, 8];
+
 
 let suma = array1.map(function (num, idx) {
     //return num + array2[idx];
+    const span = document.createElement('h2');
+
+    let parentDiv = tevas[idx].parentNode;
 
     console.log(
 
         // num + array2[idx]),
-        `NUM `, array1[idx] - array2[idx]
+        `NUM ${num} ,${idx} `, Math.round((array1[idx] / array2[idx]) * 100)
     )
+    let text = document.createTextNode(Math.round((array1[idx] / array2[idx]) * 100));
+    let text2 = document.createTextNode("% nuolaida");
+    span.append(text, text2);
+    parentDiv.insertBefore(span, tevas[idx])
+
+
+    // const span = document.createElement('h2');
+
+    // console.log(
+
+    //     // num + array2[idx]),
+    //     `NUM ${num} ,${idx} `, Math.round((array1[idx] / array2[idx]) * 100),
+    //     `sell price `, sell_price[idx]
+    // )
+    // let text = document.createTextNode(Math.round((array1[idx] / array2[idx]) * 100));
+    // span.appendChild(text);
+
+    // const li = document.createElement('li');
+    // let text = document.createTextNode(`Woohoo ${++skc}`);
+    // span.appendChild(text);
+    // base_price[idx].prepend(span);
 }
 ); // [6,8,10,12]
