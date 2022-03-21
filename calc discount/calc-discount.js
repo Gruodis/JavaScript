4ca4562458731717e8625ebbf48de3d4c531c239
Gruodis <43164261+Gruodis@users.noreply.github.com>
2022-03-21 23:19:49 +0200

    3 methods calc discount

--- calc discount/calc-discount.js
+++ calc discount/calc-discount.js
@@ -1,20 +1,29 @@
-const sell_price = document.querySelectorAll(".ispardavimas");
-const base_price = document.querySelectorAll(".bazine");
+
+let array1 = [];
+let array2 = [];
+const sell_price = document.querySelectorAll(".ispardavimas").forEach(price => {
+    array1.push(price.innerText)
+}
+);
+const base_price = document.querySelectorAll(".bazine").forEach(price => {
+    array2.push(price.innerText)
+}
+);
 const tevas = document.querySelectorAll(".akcija");
+const tevas2 = document.querySelectorAll(".ispardavimas");
 //const combine = [[sell_price], [base_price]];
 // const sell_price = parseFloat(document.querySelectorAll(".ispardavimas").innerText);
 // const base_price = parseFloat(document.querySelectorAll(".bazine").innerText);
-let array1 = [];
-let array2 = [];
 
-sell_price.forEach(price => {
-    array1.push(price.innerText)
-}
-)
-base_price.forEach(price => {
-    array2.push(price.innerText)
-}
-)
+
+// sell_price.forEach(price => {
+//     array1.push(price.innerText)
+// }
+// )
+// base_price.forEach(price => {
+//     array2.push(price.innerText)
+// }
+// )
 console.log(sell_price);
 console.log(base_price);
 console.log(tevas);
@@ -37,34 +46,40 @@ console.log(`ARRAY2 `, array2);
 // }); // [6,8,10,12]
 // console.log(sum);
 
+/***
+ * 
+ * 
+ *  Sprendimas su MAP
+ * 
+ * 
+ ***/
 
-
-let suma = array1.map(function (num, idx) {
-    //return num + array2[idx];
+let suma = array1.map(function (arrayNumber, arrayIndex) {
+    //return arrayNumber + array2[arrayIndex];
     const span = document.createElement('h2');
 
-    let parentDiv = tevas[idx].parentNode;
+    let parentDiv = tevas[arrayIndex].parentNode;
 
     console.log(
 
-        // num + array2[idx]),
-        `NUM ${num} ,${idx} `, Math.round((array1[idx] / array2[idx]) * 100)
+        // arrayNumber + array2[arrayIndex]),
+        `arrayNumber ${arrayNumber} ,${arrayIndex} `, Math.round((array1[arrayIndex] / array2[arrayIndex]) * 100)
     )
-    let text = document.createTextNode(Math.round((array1[idx] / array2[idx]) * 100));
+    let text = document.createTextNode(Math.round((array1[arrayIndex] / array2[arrayIndex]) * 100));
     let text2 = document.createTextNode("% nuolaida");
     span.append(text, text2);
-    parentDiv.insertBefore(span, tevas[idx])
+    parentDiv.insertBefore(span, tevas[arrayIndex])
 
 
     // const span = document.createElement('h2');
 
     // console.log(
 
-    //     // num + array2[idx]),
-    //     `NUM ${num} ,${idx} `, Math.round((array1[idx] / array2[idx]) * 100),
-    //     `sell price `, sell_price[idx]
+    //     // arrayNumber + array2[arrayIndex]),
+    //     `arrayNumber ${arrayNumber} ,${arrayIndex} `, Math.round((array1[arrayIndex] / array2[arrayIndex]) * 100),
+    //     `sell price `, sell_price[arrayIndex]
     // )
-    // let text = document.createTextNode(Math.round((array1[idx] / array2[idx]) * 100));
+    // let text = document.createTextNode(Math.round((array1[arrayIndex] / array2[arrayIndex]) * 100));
     // span.appendChild(text);
 
     // const li = document.createElement('li');
@@ -73,3 +88,46 @@ let suma = array1.map(function (num, idx) {
     // base_price[idx].prepend(span);
 }
 ); // [6,8,10,12]
+
+/***
+ * 
+ * 
+ *  Sprendimas su FOR
+ * 
+ * 
+ ***/
+function goWithFor(array1, array2) {
+
+    for (let i = 0; i < array1.length; i++) {
+
+        console.log(
+
+            // arrayNumber + array2[idx]),
+            `===========> GoFor fun `, Math.round((array1[i] / array2[i]) * 100)
+        )
+
+    };
+};
+goWithFor(array1, array2);
+
+
+/***
+ * 
+ * 
+ *  Sprendimas su ForEach
+ * 
+ * 
+ ***/
+
+function goWithForEach(array1, array2) {
+
+    array1.forEach((arrayNumber, arrayIndex, smth) => {
+        const num2 = array2[arrayIndex];
+        console.log(
+
+            // num + array2[idx]),
+            `~~~~~~~~>  ${arrayNumber}, ${arrayIndex}, smth ${smth} GoEach fun `, Math.round((arrayNumber / num2) * 100)
+        )
+    });
+};
+goWithForEach(array1, array2);
